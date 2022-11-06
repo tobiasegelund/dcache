9938b25a50c99177299e7c7525fda931a7844b7e
Tobias Egelund <egelundtobias@gmail.com>
2022-11-06 15:30:04 +0100

    Add requirement to handle parquets

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,9 +1,15 @@
-# from pathlib import Path
+import pytest
 from dcache._fs import save_to_file, load_file
+
+import pandas as pd
 
 
 def test_save_to_file(tmp_path):
     save_to_file(filename=tmp_path.joinpath("test_save_to_file"), data=2)
+
+
+def test_save_to_file_w_df(tmp_path):
+    save_to_file(filename=tmp_path.joinpath("test_save_to_file"), data=pd.DataFrame())
 
 
 def test_load_file():
